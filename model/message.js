2c521aaef427587434fbc0fbe3a2726416e713e6
naknamu <jodeldelvalle@gmail.com>
2023-04-19 23:29:13 +0800

    Testing luxon date format

--- model/message.js
+++ model/message.js
@@ -27,7 +27,7 @@ const MessageSchema = new Schema({
 });
 
 MessageSchema.virtual("date_posted_formatted").get(function () {
-  return DateTime.fromJSDate(this.date_posted).toJSDate()
+  return this.date_posted.toLocaleString('en-CA')
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
