714790619376df8de6f1e6735420c848d3d53827
naknamu <jodeldelvalle@gmail.com>
2023-04-20 13:03:21 +0800

    Changed date formatting

--- model/message.js
+++ model/message.js
@@ -27,7 +27,7 @@ const MessageSchema = new Schema({
 }, { timestamps : true});
 
 MessageSchema.virtual("date_posted_formatted").get(function () {
-  return DateTime.fromJSDate(this.date_posted).toLocaleString(DateTime.DATETIME_MED);;
+  return new Date(this.date_posted);
 });
 
 
